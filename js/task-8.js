30badb796bb90c59673c6bf739e5ebcda30aad6c
Valeriy Mashura <dr.mashura@gmail.com>
2020-12-20 19:10:00 +0200

    Update task-8.js

--- js/task-8.js
+++ js/task-8.js
@@ -13,3 +13,27 @@
 // Размеры самого первого div - 30px на 30px
 // Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 // Создай функцию destroyBoxes(), которая очищает div#boxes
+const divContolRef = document.querySelector('#controls');
+const boxesRef = document.querySelector('#boxes');
+
+const randomRGB = () => Math.floor(Math.random() * 254);
+const r = randomRGB();
+const g = randomRGB();
+const b = randomRGB();
+const inputRef = document.querySelector('#controls input');
+const amount = inputRef.value;
+
+divContolRef.addEventListener('click', event => {
+target
+
+})
+
+const createBoxes = amount => {
+  for (let i = 0; i < amount; i += 1) {
+    const newDivRef = document.createElement('div');
+    newDivRef.setAttribute('style', `background-color: rgb(${r}, ${g}, ${b}); width: 30px; height: 30px;`);
+    boxesRef.appendChild(newDivRef);
+  }
+};
+
+createBoxes(3);
